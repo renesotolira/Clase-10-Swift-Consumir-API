dab8c7fc199cf6a1e0abd2508bbcfa947f4cde26
René <rene.soto01@iest.edu.mx>
2023-11-05 20:03:52 -0600

    Initial Commit

---
+++ Api/ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiApp.swift
+//  Api
+//
+//  Created by Rene SL on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
